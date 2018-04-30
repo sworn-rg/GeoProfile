48d9ecc095bf3d0e6fb8937ad89a7ffc75f6416e
Roger Gill <roger.gill@moneysupermarket.com>
2018-04-30 10:48:07 +0100

    Adding scripts

---
+++ Helpers/db_abstraction.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 10:47:04 2018
+
+@author: roger.gill
+"""
+
+weqwe
---
+++ scripts/daily_run.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 10:46:19 2018
+
+@author: roger.gill
+"""
+
