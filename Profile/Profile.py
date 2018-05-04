dc144dce469df6e7dd50b15c6e1bc8cdd40f08f6
Roger Gill <roger.gill@moneysupermarket.com>
2018-05-04 12:07:38 +0100

    Profile stub class

--- Profile/Profile.py
+++ Profile/Profile.py
@@ -8,40 +8,20 @@ from datetime import datetime
 from geopy.distance import vincenty
 import statistics as st
 
-class Location:
-    """An Activity: Base class for geo activity data event
+class Profile:
+    """A Profile: Base class for geo profile
     Attributes:
     
     """
 
-    def __init__(self, activity_cluster, from_json = False):
+    def __init__(self, location_list):
         """ Basic checks and conditions for class constructors """
-        self.activity_cluster = activity_cluster
-        self.n_activities = len(self.activity_cluster)
+        self.location_list = location_list
+        self.n_locations = len(location_list)
         
-        if len(self.activity_cluster) < 1:
+        if len(self.location_list) < 1:
             raise Exception('A location requires 1 Activities')
-        
-        
-        self.abstract_activities()
-        self.location_summary()
     
-    def abstract_activities(self):
-        self.datetimes = []
-        self.longitudes = []
-        self.latitudes = []
-        self.courses = []
-        for i in range(self.n_activities):
-            self.datetimes.append(self.activity_cluster[i].end_datetime)
-            self.latitudes.append(self.activity_cluster[i].end_latitude)
-            self.longitudes.append(self.activity_cluster[i].end_longitude)
-
-    def location_summary(self):
-        self.first_activity = min(self.datetimes).strftime("%Y-%m-%d")
-        self.last_activity = max(self.datetimes).strftime("%Y-%m-%d")
-    
-    def location_time_summary(self):
-        self.n_activities = len(self.activity_cluster)
     
     def location_activity_summary(self):
         return 'blahhh'
